cd6ddae77b0ca0909c18e699a4de24e9216da386
Kalvin <ThatGuyKalvin@users.noreply.github.com>
2023-06-27 20:43:53 +1000

    Update main.tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "${var.app_name}${local.environment_short_name}-sa"
+  name                     = format("%s%ssa", var.app_name, local.environment_short_name)
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
 
