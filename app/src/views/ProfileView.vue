72ea9fa6cda26b2d9e19a9bee896836a2651bb9c
Kalvin <ThatGuyKalvin@users.noreply.github.com>
2023-08-10 23:49:18 +1000

    Add socials

--- app/src/views/ProfileView.vue
+++ app/src/views/ProfileView.vue
@@ -1,22 +1,54 @@
 <script setup lang="ts">
-import Image from 'primevue/image';
+import Image from 'primevue/image'
+import Avatar from 'primevue/avatar'
 </script>
 
 <template>
   <div class="profile">
-    <h1>Profile</h1>
     <div class="circular-image" >
         <Image src="./images/headshot.jpg" alt="Headshot" width="250" />
+    </div>
+    <div class="socials">
+      <v-row justify="center">
+        <v-col cols="4" xs="1" md="1">
+          <v-btn class="social-btn" href="https://github.com/ThatGuyKalvin" target="_blank" size="x-small" icon="pi pi-github" variant="flat"></v-btn>
+        </v-col>
+        <v-col cols="4" xs="1" md="1">
+          <v-btn class="social-btn" href="https://learn.microsoft.com/en-gb/users/kalvinjohnston/transcript/vyqrgi0xqkme9jl" target="_blank" size="x-small" icon="pi pi-microsoft" variant="flat"></v-btn>
+        </v-col>
+        <v-col cols="4" xs="1" md="1">
+          <v-btn class="social-btn" href="https://linkedin.com/in/kalvin-johnston/" target="_blank" size="x-small" icon="pi pi-linkedin" variant="flat"></v-btn>
+        </v-col>
+        <v-col cols="4" xs="1" md="1">
+          <v-btn class="social-btn"  href="mailto:kalvin4467@hotmail.co.uk" target="_blank" size="x-small" icon="pi pi-envelope" variant="flat"></v-btn>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
 
 <style scoped>
+.profile {
+  padding-top: 12px;
+}
+.socials {
+  padding-top: 12px;
+}
+
+.social-btn {
+  color: var(--text-color-secondary)
+}
+
+.icons {
+  color: var(--text-color);
+  background: none;
+}
+
 .circular-image {
-  width: 250px; /* Set the desired width */
-  height: 250px; /* Set the desired height */
-  border-radius: 50%; /* Creates a circular shape */
-  overflow: hidden; /* Ensures the image stays within the circle */
-  margin: 0 auto; /* Center the circle */
+  width: 250px;
+  height: 250px;
+  border-radius: 50%;
+  overflow: hidden;
+  margin: 0 auto;
 }
 </style>
