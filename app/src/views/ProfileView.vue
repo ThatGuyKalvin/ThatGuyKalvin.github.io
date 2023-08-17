90284ad1aaba2e3df1fe9ffa24f3b0c962ddf38e
Kalvin <ThatGuyKalvin@users.noreply.github.com>
2023-08-18 01:04:21 +1000

    Use pdfjs-dist

--- app/src/views/ProfileView.vue
+++ app/src/views/ProfileView.vue
@@ -9,19 +9,19 @@ import Image from 'primevue/image'
     </div>
     <div class="socials">
       <v-row justify="center">
-        <v-col cols="12" sm="1" md="1">
+        <v-col cols="4" sm="1">
           <v-btn class="social-btn" href="https://github.com/ThatGuyKalvin" target="_blank" size="x-small" icon="pi pi-github" variant="flat" alt="GitHub" />
         </v-col>
-        <v-col cols="12" sm="1" md="1">
+        <v-col cols="4" sm="1">
           <v-btn class="social-btn" href="https://learn.microsoft.com/en-gb/users/kalvinjohnston/transcript/vyqrgi0xqkme9jl" target="_blank" size="x-small" icon="pi pi-microsoft" variant="flat" alt="Microsoft Learn"/>
         </v-col>
-        <v-col cols="12" sm="1" md="1">
+        <v-col cols="4" sm="1">
           <v-btn class="social-btn" href="https://linkedin.com/in/kalvin-johnston/" target="_blank" size="x-small" icon="pi pi-linkedin" variant="flat" alt="LinkedIn" />
         </v-col>
-        <v-col cols="12" sm="1" md="1">
+        <v-col cols="4" sm="1">
           <v-btn class="social-btn"  href="mailto:kalvin4467@hotmail.co.uk" target="_blank" size="x-small" icon="pi pi-envelope" variant="flat" alt="Email" />
         </v-col>
-        <v-col cols="12" sm="1" md="1">
+        <v-col cols="4" sm="1">
           <v-btn class="social-btn"  href="https://app.pluralsight.com/profile/kalvin-johnston" target="_blank" size="x-small" icon variant="flat" alt="Pluralsight">
             <v-icon>
               <img src="@/assets/icons/pluralsight.svg" />
--- app/src/views/ResumeView.vue
+++ app/src/views/ResumeView.vue
@@ -1,14 +1,72 @@
 <template>
   <div class="resume">
-    <object class="pdf" :data="pdfUrl" type="application/pdf" width="100%">
-      <p>This browser does not support PDFs. You can download the PDF <a :href="pdfUrl">here</a>.</p>
-    </object>
+    <v-row justify="center">
+      <div class="pdf-container">
+        <canvas ref="pdfCanvas"></canvas>
+      </div>
+    </v-row>
+    <v-row justify="center">
+      <div class="navigation-controls">
+        <v-btn @click="prevPage" :disabled="currentPage === 1" icon="pi pi-chevron-circle-left" variant="flat" size="x-small" />
+        <span>Page {{ currentPage }} of {{ numPages }}</span>
+        <v-btn @click="nextPage" :disabled="currentPage === numPages" icon="pi pi-chevron-circle-right" variant="flat" size="x-small" />
+        <v-btn :href="pdfUrl" target="_blank" icon="pi pi-cloud-download" variant="flat" size="x-small" />
+      </div>
+    </v-row>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import * as pdfjsLib from 'pdfjs-dist';
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.9.179/pdf.worker.min.js'
+
 const pdfUrl = ref('pdfs/Kalvin_Johnston_Resume.pdf')
+const pdfCanvas = ref<HTMLCanvasElement | null>(null)
+
+const currentPage = ref(1)
+let numPages = 2
+
+const loadPdf = async () => {
+  if (pdfCanvas.value) {
+    const loadingTask = pdfjsLib.getDocument(pdfUrl.value);
+    const pdfDocument = await loadingTask.promise;
+    numPages = pdfDocument.numPages;
+
+    const page = await pdfDocument.getPage(currentPage.value);
+    const viewport = page.getViewport({ scale: 1 });
+
+    const context = pdfCanvas.value.getContext('2d') as CanvasRenderingContext2D;
+    pdfCanvas.value.width = viewport.width;
+    pdfCanvas.value.height = viewport.height;
+
+    const renderContext = {
+      canvasContext: context,
+      viewport: viewport,
+    };
+    await page.render(renderContext);
+  }
+}
+
+const prevPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value -= 1;
+    loadPdf()
+  }
+}
+
+const nextPage = () => {
+  if (currentPage.value < numPages) {
+    currentPage.value += 1;
+    loadPdf()
+  }
+}
+
+onMounted(() => {
+  loadPdf()
+})
+
 </script>
 
 <style scoped>
@@ -20,7 +78,18 @@ const pdfUrl = ref('pdfs/Kalvin_Johnston_Resume.pdf')
   box-sizing: border-box; /* Include padding in height calculation */
 }
 
-.pdf {
+.pdf-container {
   height: 83vh; /* Set the height to the full viewport height */
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.navigation-controls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 8px;
 }
 </style>
