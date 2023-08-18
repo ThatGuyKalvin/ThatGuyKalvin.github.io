fb4b3b73b592dbd928430649864aebeb89588170
Kalvin <ThatGuyKalvin@users.noreply.github.com>
2023-08-18 21:08:40 +1000

    Blue theme

--- app/src/main.ts
+++ app/src/main.ts
@@ -7,7 +7,7 @@ import App from './App.vue'
 import PrimeVue from 'primevue/config'
 import router from './router'
 
-import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/themes/lara-light-blue/theme.css";
 import "primevue/resources/primevue.min.css";
 
 // Vuetify
